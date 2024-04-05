c89f22d6cb16122e0699e4f25eb0ed3e1c11832f
Tez888 <164473188+Tez888@users.noreply.github.com>
2024-04-05 15:46:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 #include "startgame.h"
 #include <random>
 
+enum class Suit = {CLUB, DIAMOND, HEART, SPADE};
+enum class Rank = {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+
+struct Card = {
+    Suit suit;
+    Rank rank;
+};
+
+class Deck {
+public:
+
+private:
+    static const int NUM_CARDS = 52;
+    Card cards[NUM_CARDS];
+    int currentCardindex;
+}
+
 int main()
 {
     initializegame();

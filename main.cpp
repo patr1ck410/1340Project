a13753ca68cf25401a92b18cdcdbd142791dfc93
Tez888 <164473188+Tez888@users.noreply.github.com>
2024-04-05 16:35:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,40 @@
 #include "initialization.h"
 #include "startgame.h"
 #include <random>
+#include <cstdlib>
+#include <time.h>
+
+
+enum class Suit = {CLUB, DIAMOND, HEART, SPADE};
+enum class Rank = {TWO, THREE, FOUR, FIVE, SIX, SEVEN, RIGHT, NINE, TEN, JACK, QUEEN, ACE};
+
+struct Card{
+    Suit suit;
+    Rank rank;
+};
+
+class Deck{
+public:
+    void setupdeck(){
+        int index = 0;
+        for (int suit = 0; suit < 4; suit++){
+            for (int rank = 0; rank < 13; rank++){
+                cards[index++] = {static_cast<Suit>(suit), static_cast<Rank>(rank)};
+            }
+        }
+    }
+    void shuffle(){
+        srand(time(0));
+    }
+private:
+    static const int NumOfCards = 52;
+    Card cards[NumOfCards];
+};
 
 int main()
 {
     initializegame();
     startgame();
+    Deck deck;
+    deck.setupcards();
 }

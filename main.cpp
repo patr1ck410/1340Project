fff9b2f5446e754f57b6053d87ce69ad768db563
henri311 <henri311@connect.hku.hk>
2024-04-07 17:29:06 +0800

    update main.cpp
    build player struct ( linked list)

--- main.cpp
+++ main.cpp
@@ -6,21 +6,36 @@
 #include <string>
 #include <fstream>
 #include <time.h>
-struct 
+struct player{
+	string name;
+	double chips;
+	bool ingame;
+	int hand[][];
+	double chipsput;
+	bool allin;
+	player *next;
+};
+	 
 int main()
 {
 	ifstream data;
 	data.open("lastgame.txt") // lastgame.txt saves last game details
 	double poolsize;
-	int numbernum;
+	int playernum;
+	bool newgame;
 	if (data.fail()==False){    // lastgame.txt exists
 		cout << "previous game dedected . Retrieve ? (1=Yes/0=No)" <<endl;
 		bool ans;
 		cin >> ans;
-		if (ans==true)
+		if (ans==true){
 			retrievegame();//not done
+			newgame=false;
+		}
 	}
-			 
+	if (newgame==false){
+		cout << "Number of players?" << endl;
+		cin >> playernum;
+				 
 	initializegame();
 	startgame();
 	Deck deck;

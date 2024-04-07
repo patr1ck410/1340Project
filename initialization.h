5099972968a7cc8082523e27cdaf488c84e7e33f
Michaelchann <164472966+Michaelchann@users.noreply.github.com>
2024-04-08 00:56:24 +0800

    Update initialization.h

--- initialization.h
+++ initialization.h
@@ -2,8 +2,18 @@
 #define INITIALIZATION_H
 
 void initializegame();
-void printwelcome();
-void printoption();
+  
+void printwelcome(bool ans); {
+  if ans == false {
+    cout << "Welcome to the new game!" << endl
+  }
+  else {
+    cout << "Welcome back!" << endl
+  }
+}
+void printoption(player p);
+  
+  
 
 
 #endif

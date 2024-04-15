39028929c326e2037a28bde6cc322271ef6a6ab2
patr1ck410 <patr1ck@connect.hku.hk>
2024-04-15 15:42:35 +0800

    finished retrieve function

--- retrieve.cpp
+++ retrieve.cpp
@@ -1,26 +1,35 @@
-#include "retrieve.h"
+#include "loadandsave.h"
 #include "structures.h"
+#include "insert.h"
 #include <fstream>
 #include <sstream>
 using namespace std;
-void retrievegame(player  *& playerlist, int & playernum)
+void retrievegame(player  *& head, player *& tail, int & playernum)
 {
     ifstream lastgame;
     lastgame.open("lastgame.txt");
-    string data;
+    string line;
     int i =0;
-    while (getline(lastgame, data))
+    while (getline(lastgame, line))
     {
-        istringstream iss(data);
-        string temp[2];
-        int j = 0;
-        while (iss >> data)
-        {
-            temp[j] = data;
-            j++;
+        istringstream iss(line);
+        string name, chips;
+        iss >> name >> chips;
+        player * p = new player;
+        p -> name = name;
+        p -> chips = stod(chips);
+        p -> ingame = true;
+        p -> chipsput = 0;
+        p -> allin = false;
+        p -> next = NULL;
+        if (head= NULL){
+            head = p;
+            tail = p;
         }
-        playerlist[i].name = temp[0];
-        playerlist[i].chips = stoi(temp[1]);
-        i++;
+        else {
+            tail -> next = p;
+            tail = p ;
+    }
+    p->next = head;
     }
 };
--- retrieve.h
+++
@@ -1,8 +0,0 @@
-#ifndef RETRIEVE_H
-#define RETRIEVE_H
-struct player;
-
-
-void retrievegame(player  *& playerlist, int & playernum);
-
-#endif

f361e706d21b42752b47f3afa384cf484d4e15e7
Tez888 <164473188+Tez888@users.noreply.github.com>
2024-04-17 12:43:16 +0800

    Update tezindrawcard.cpp

--- tezindrawcard.cpp
+++ tezindrawcard.cpp
@@ -1,24 +1,14 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 
-struct Card{
-   string suit;
-   string rank;
- 10 };
- 11 
- 12 void drawcard(Card deck[], int &decksize){
- 13   srand(time(0));
- 14   int randomindex = rand() % decksize;
- 15   Card drawncard = deck[randomindex];
- 16   cout << "You drew" << drawncard.suit << drawncard.rank << endl;
- 17   deck[randomindex] = deck[decksize - 1];
- 18   decksize--;
- 19 }
- 20 
- 21 int main(){
- 22   string suits[] = {"Spades", "Hearts", "Diamonds", "Clubs"};
-    @                                                                           
-                                                              1,1           Top
+void cardDraw(Deck &deck, int &suit, int &rank){
+   srand(time(NULL));
+   int randomsuit = rand() % 3;
+   int randomrank = rand() % 12;
+   if(deck[randomsuit][randomrank]){
+      
+   }
+} 
+                                                      

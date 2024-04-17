f9f02cccfe44066e699e5235bc8df493ed0f7c26
Tez888 <164473188+Tez888@users.noreply.github.com>
2024-04-17 13:00:13 +0800

    Update tezindrawcard.cpp

--- tezindrawcard.cpp
+++ tezindrawcard.cpp
@@ -8,11 +8,11 @@ using namespace std;
 void cardDraw(Deck &deck, player* head, int &suit, int &rank){
    srand(time(NULL));
    while(head != nullptr){
-      int randomsuit = rand() % 3;
-      int randomrank = rand() % 12;
+      int randomsuit = rand() % 4;
+      int randomrank = rand() % 13;
       while(!deck[randomsuit][randomrank]){
-         int randomsuit = rand() % 3;
-         int randomrank = rand() % 12;
+         int randomsuit = rand() % 4;
+         int randomrank = rand() % 13;
          head->hand[randomsuit][randomrank];
          head = head->next;
       }

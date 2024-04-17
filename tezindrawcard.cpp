63207a77a1174e57bb77cf5bbfe265698ea959ad
Tez888 <164473188+Tez888@users.noreply.github.com>
2024-04-17 13:57:25 +0800

    Update tezindrawcard.cpp

--- tezindrawcard.cpp
+++ tezindrawcard.cpp
@@ -5,13 +5,16 @@
 #include "structures.h"
 using namespace std;
 
-void cardDraw(Deck &deck, int &card[][]){
+void cardDraw(Deck &deck, int &card[])
+{
    srand(time(NULL));
    bool exists = false;
-   while(!exists){
+   while(!exists)
+   {
       int randomsuit = rand() % 4;
       int randomrank = rand() % 13;
-      if(deck[randomsuit][randomrank]){
+      if(deck[randomsuit][randomrank])
+      {
          exists = true;
          card[0] = randomsuit;
          card[1] = randomrank;
@@ -19,12 +22,19 @@ void cardDraw(Deck &deck, int &card[][]){
          return;
       }
    }
+}
 
-void distribute(Deck &deck, player *button, int playernum){
-   for(int i = 0; i < playernum * 2; i++){
-      cardDraw(deck, int &card[][]);
-      button->hand = [card[0]][card[1]];
-      button = button->next;
+void distribute(Deck &deck, player *button, int playernum)
+{
+   for(int i = 0; i < playernum ; i++)
+   {
+      for (int j =0; j<2 ;j++)
+      {
+         cardDraw(deck, int &card[]);
+         button->hand[j][0] = card[0] ; //suit
+         button->hand[j][1] = card[1] ; //rank
+         button = button->next;
+      }
    }
 }
 

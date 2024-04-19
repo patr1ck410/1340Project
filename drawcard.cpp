a2d6741e2fdda0661b15555d622c0c7e682a2780
Henriii <158246891+henri311@users.noreply.github.com>
2024-04-20 00:12:53 +0800

    Update drawcard.cpp
    remove unused part

--- drawcard.cpp
+++ drawcard.cpp
@@ -39,21 +39,4 @@ void distribute(bool (&deck)[4][13], player *button, int playernum)
    }
 }
 
-void printcard(int card[2])
-{   
-   string suits[] = {"Diamond", "Club", "Heart", "Spade"};
-   string ranks[] = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-   int suit = card[0];
-   int rank = card[1];
-   cout << suits[suit] << " " << ranks[rank] << endl;
-}
-
-void showhand(player *button)
-{
-   for(int i = 0; i < 2; i++)
-   {
-      printcard(button->hand[i]);
-   }      
-}
-
 

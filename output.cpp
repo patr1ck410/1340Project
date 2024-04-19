55d5cd0858f08bb05008b592e7835c1a30fe1b5a
henri311 <henri311@connect.hku.hk>
2024-04-20 00:32:28 +0800

    update output.cpp
    better now

--- output.cpp
+++ output.cpp
@@ -2,19 +2,30 @@
 using namespce std;
 void printcard(int card[2])
 {   
-   string suits[] = {"Diamond", "Club", "Heart", "Spade"};
-   string ranks[] = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-   int suit = card[0];
-   int rank = card[1];
-   cout << suits[suit] << " " << ranks[rank] << endl;
+	string suits[] = {"Diamond", "Club", "Heart", "Spade"};
+	string ranks[] = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	int suit = card[0];
+	int rank = card[1];
+	cout << suits[suit] << " " << ranks[rank] ;
 }
 
 void showhand(player *individual)
 {
-   for(int i = 0; i < 2; i++)
-   {
-      printcard(button->hand[i]);
-   }      
+	cout << "Your hands are " ;
+	for(int i = 0; i < 2; i++){ 
+   		printcard(individual->hand[i]);
+		if (i!=1)
+			cout << ", ";
+   	}
+	cout << endl;
 }
-void showpublic(
+void showpublic(int cards[5][2],int num){
+	cout << "Community "<< num << "cards are" ; 
+	for (int i=0;i < num;i++){
+		printcard(cards[i]);
+		if (i!=num-1)
+			cout << ", ";
+	}
+	cout << endl;
+}
 

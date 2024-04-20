de5602aea2c56eb522f3cfe2e6c2a3977dc44dde
henri311 <henri311@connect.hku.hk>
2024-04-20 16:31:07 +0800

    update checkwin.cpp
    breifly write the logic

--- checkwin.cpp
+++ checkwin.cpp
@@ -2,13 +2,23 @@
 #include <string>
 using namespace std;
 
-void checkwin(player * button) { //check which type of poker hand player have
+void checkwin(player * button,publiccard) { //check which type of poker hand player have
 	player * current = button;
 	do {
 		if (current ->ingame){
-		`		
-
-
+			int combine[7][2];
+			for (int i=0;i<5;i++){
+				for (int j=0 ;j<2;j++){
+					combine[i][j]=publiccard[i][j];
+				}
+			}
+			for (int i=5;i<7;i++){
+				for (int j-0;j<2;j++){
+					combine[i][j]=current->hand[i-5][j];
+				}
+			}
+			current->value=assignvalue(combine));		
 		current = curent -> next;
 	}while(current!= button);
 }
+int assignvalue(
--- structures.h
+++ structures.h
@@ -10,7 +10,6 @@ struct player{
 	double chipsput; // stroing deadn chips in front
 	bool allin; // true indicates the player has all in
 	int sidepool; // only have value when a player all in and determine the pot he can get if he wins
-	int handranking[7][2]; // combining ht ecommunity cards and hand cards
 	int value; // assigning a value of the rank of player after check win 
 	player *next;   //circular linked list
 };

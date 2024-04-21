52b5bcc436a95fc37cec26f647f4aaa2bd7bbca2
Michaelchann <164472966+Michaelchann@users.noreply.github.com>
2024-04-21 23:14:48 +0800

    Update minigame.cpp

--- minigame.cpp
+++ minigame.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
-#include <>
+#include <ctime>
+#include <cstdlib>
+#include "initialization.h"
+#include "structures.h"
+#include <string>
+using namespace std;
+
+void cardDraw(bool (&deck)[4][13], int card[])
+{
+   srand(time(NULL));
+   bool flag = false;
+   while(!flag)
+   {
+      int randomsuit = rand() % 4;
+      int randomrank = rand() % 13;
+      if(deck[randomsuit][randomrank])
+      {
+         flag = true;
+         card[0] = randomsuit;
+         card[1] = randomrank;
+         deck[randomsuit][randomrank] = false;
+      }
+   }
+}
+cardDraw();

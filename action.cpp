275a5b8baf32dd4051d7ffa13299525b7d35a162
Henriii <158246891+henri311@users.noreply.github.com>
2024-04-22 21:57:36 +0800

    Update action.cpp
    debug

--- action.cpp
+++ action.cpp
@@ -97,7 +97,6 @@ void action (player *button , double &poolsize ,int playernum,int &playerleft ,b
 		for (int i = 0 ; i < playernum ; i++) // checking if all players have put same amount of chips (max) , or have all in 
 		{
 			if ((checking->ingame == true && checking -> allin==false && checking ->chipsput < max)|| (current==button->next->next && current->chipsput==1)){// consider the start turn that big blind still have actions 
-				cout << checking->name << checking -> ingame << checking -> allin << checking ->chipsput <<max ;
 		 		endturn=false;
 				cout << i;
 				break;

9f81c505d911240cd5e668c8735f08dccdd07448
Henriii <158246891+henri311@users.noreply.github.com>
2024-04-23 13:22:45 +0800

    Update checkwin.cpp

--- checkwin.cpp
+++ checkwin.cpp
@@ -36,7 +36,7 @@ void checkwin(player * button, int publiccard[5][2]) //check which type of poker
 int assignvalue(int combine[7][2]){
 	int suit[4]={0,0,0,0};
 	int rank [13]={0,0,0,0,0,0,0,0,0,0,0,0,0}; // staticial data of the cards
-	int value;
+	int value=0;
 	for (int i =0;i<7;i++)
 		suit[combine[i][0]]+=1;
 	for (int i =0; i<7;i++)
@@ -52,7 +52,7 @@ int assignvalue(int combine[7][2]){
 			count+=1;
 		else 
 			count=0;
-		if (count==5)
+		if (count>=5)
 			straight=i; // if straight !=14 , then exist straight , the value straight representing is useful in assigning values
 	}
 	vector <int> suits;
@@ -64,17 +64,20 @@ int assignvalue(int combine[7][2]){
 		}
 		sort(suits.begin(), suits.end()); // sorted vector for ranks in same flush
 	}
-	if (suited !=4 && straight !=14) //possibility in striaght flush
+	if (suited !=4 && straight !=14){ //possibility in striaght flush
 		int count=1;
 		int prev=suits[0];
 		for (int i=1;i<suits.size();i++){
 			if (suits[i]==(prev+1)){
 				count+=1;
 				prev=suits[i];
+			}
 			else
 				count=0;
+			if (count>=5)
+				value=
 		}
-		
+	}
 	
 
 	return value;

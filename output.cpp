052ffdee0fed3da0b3bdd6c624ff967f396c9075
Tez888 <tezin888@connect.hku.hk>
2024-04-27 21:54:03 +0800

    update output

--- output.cpp
+++ output.cpp
@@ -13,7 +13,7 @@ void printcard(int suit, int rank)
 
 void showhand(player *individual)
 {
-	cout << "Your hands are ";
+	cout << "Your hands are    ";
 	for (int i = 0; i < 2; i++)
 	{
 		printcard(individual->hand[i][0], individual->hand[i][1]);
@@ -24,7 +24,7 @@ void showhand(player *individual)
 }
 void showpublic(int cards[5][2], int num)
 {
-	cout << "Community " << num << " cards are ";
+	cout << "Community " << num << " cards are    ";
 	for (int i = 0; i < num; i++)
 	{
 		printcard(cards[i][0], cards[i][1]);

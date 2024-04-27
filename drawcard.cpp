b05c28a88e5f7a20f89739767ebea5f764377b8a
henri311 <henri311@connect.hku.hk>
2024-04-27 11:51:54 +0800

    fixed bug of same card

--- drawcard.cpp
+++ drawcard.cpp
@@ -10,15 +10,14 @@ using namespace std;
 void cardDraw(bool (&deck)[4][13], int (&card)[2])
 {
    srand(time(NULL));
-   bool flag = false;
-   string suits[] = {"\u2660", "\u2663", "\u2665", "\u2666"};
-   while(!flag)
+   bool found = false;
+   while(!found)
    {
       int randomsuit = rand() % 4;
       int randomrank = rand() % 13;
       if(deck[randomsuit][randomrank])
       {
-         flag = true;
+         found = true;
          card[0] = randomsuit;
          card[1] = randomrank;
          deck[randomsuit][randomrank] = false;
@@ -28,18 +27,16 @@ void cardDraw(bool (&deck)[4][13], int (&card)[2])
 
 void distribute(bool (&deck)[4][13], player * button, int playernum)
 {
-   for(int i = 0; i < playernum; i++)
-   {
-      for(int j = 0; j < 2; j++)
-      {
-         int card[2];
-         cardDraw(deck, card);
-         cout << card[0] << "  " << card[1] << endl;
-         button->hand[j][0] = card[0];
-         button->hand[j][1] = card[1];
-         button = button->next;
-      }
-   }
+	player * current = button;
+	for(int i = 0; i < playernum; i++){
+		for(int j = 0; j < 2; j++) {
+ 			int card[2];
+  			cardDraw(deck, card);
+			current->hand[j][0] = card[0];
+			current->hand[j][1] = card[1];
+		}
+		current= current-> next;
+	}
 }
 
 
--- output.cpp
+++ output.cpp
@@ -4,20 +4,18 @@
 #include "structures.h"
 using namespace std;
 
-void printcard(int card[2])
+void printcard(int suit ,int rank)
 {   
 	string suits[] = {"\u2666", "\u2663", "\u2665", "\u2660"};
 	string ranks[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	int suit = card[0];
-	int rank = card[1];
 	cout << suits[suit] << " " << ranks[rank] ;
 }
 
 void showhand(player *individual)
 {
 	cout << "Your hands are " ;
-	for(int i = 0; i < 2; i++){ 
-   		printcard(individual->hand[i]);
+	for(int i = 0; i < 2; i++){
+   		printcard(individual->hand[i][0],individual->hand[i][1]);
 		if (i!=1)
 			cout << ", ";
    	}
@@ -26,7 +24,7 @@ void showhand(player *individual)
 void showpublic(int cards[5][2],int num){
 	cout << "Community "<< num << " cards are" ; 
 	for (int i=0;i < num;i++){
-		printcard(cards[i]);
+		printcard(cards[i][0],cards[i][1]);
 		if (i!=num-1)
 			cout << ", ";
 	}

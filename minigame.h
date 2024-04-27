5273870c3ce61a9ae5b84ae01339f02d34f031a1
patr1ck410 <patr1ck@connect.hku.hk>
2024-04-28 00:40:49 +0800

    debugged

--- minigame.h
+++ minigame.h
@@ -4,7 +4,7 @@
 #include "structures.h"
 
 void calodds(double (&odds)[3], int (&card)[2], int (&sto)[1]);
-int minigame(bool (&deck)[4][13],player * current);
+void minigame(bool (&deck)[4][13],player * current);
 void checkchips(bool (&deck)[4][13], player * button, int playernum);
 
 #endif
